af65089b4624d407126eaf45fcb95d75fc68d4b9
RosanaAlves <rosanaalvesmolina7@gmail.com>
2023-07-19 21:56:26 -0300

    Criando projeto

---
+++ R/Limpeza de dados.R
@@ -0,0 +1,12 @@
+
+# Bibliotecas -------------------------------------------------------------
+
+library(readr)
+
+
+
+# Carregando arquivo ------------------------------------------------------
+
+CopyOfCustomer_Churn_Records <- read_csv("Data-Raw/CopyOfCustomer-Churn-Records.csv")
+View(CopyOfCustomer_Churn_Records)
+
