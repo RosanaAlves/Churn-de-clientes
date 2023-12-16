bb27232a30eb818441180e7007fa1a5f07375c4d
RosanaAlves <rosanaalvesmolina7@gmail.com>
2023-12-15 22:13:04 -0300

    Update Analise de predição.R

--- Data-Raw/Analise de predição.R
+++ Data-Raw/Analise de predição.R
@@ -6,32 +6,46 @@ library(patchwork)
 # Leitura da base de dados
 base  <- read_csv("Data-Raw/Customer-Churn-Records.csv")
 
-# Apaga a coluna clientid
-base$CustomerId = NULL
+# Verificação das variaveis
 
-# # Valores inconsistentes
-# base$age = ifelse(base$age < 0, 40.92, base$age)
+summary(base)
 
-# # Valores faltantes
-# base$age = ifelse(is.na(base$age), mean(base$age, na.rm = TRUE), base$age)
+# retirando as variaveis de ID
 
-# Escalonamento
-base[, 1:3] = scale(base[, 1:3])
+base <- base[,4:18]# Os dados que ficaram fazen sentido para a análise
+
+# Preparando a base para treinar um modelo --------------------------------
+## Transformação de dados categóricos
+
+base$Geography <- factor(base$Geography, levels = c("Spain", "Germany","France"), labels = c(1,2,3))
+base$Gender <- factor(base$Gender, levels = c("Male","Female"), labels = c(0,1))
+base$`Card Type` <- factor(base$`Card Type`, levels = c("DIAMOND","GOLD","PLATINUM","SILVER"), labels = c(1,2,3,4))
+
+categ <- c("Geography", "Gender", "Card Type")
+data1 <- base %>% select(categ)
+data2 <- base %>% select(-categ)
+## Escalonamento
+
+data2[, 1:12] = scale(data2[, 1:12])
+
+base <- data.frame(data1, data2)
+
+# -------------------------------------------------------------------------
 
 # Divis?o entre treinamento e teste
 library(caTools)
 
 set.seed(1)
-divisao = sample.split(base$income, SplitRatio = 0.75)
+divisao = sample.split(base$Exited, SplitRatio = 0.75)
 base_treinamento = subset(base, divisao == TRUE)
 base_teste = subset(base, divisao == FALSE)
 
-classificador = glm(formula = default ~ ., family = binomial, data = base_treinamento)
-probabilidades = predict(classificador, type = 'response', newdata = base_teste[-4])
+classificador = glm(formula = Exited ~Age+Complain+Point.Earned, family = gaussian , data = base_treinamento)
+probabilidades = predict(classificador, type = 'response', newdata = base_teste[-12])
 previsoes = ifelse(probabilidades > 0.5, 1, 0)
-matriz_confusao = table(base_teste[, 4], previsoes)
+matriz_confusao = table(base_teste[, 12], previsoes)
 library(caret)
 confusionMatrix(matriz_confusao)
 
 # ZeroR 
-table(base_teste$default)
+table(base_teste$Exited)
